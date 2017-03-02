5e82171621f98dd1efbc8347745fce79c6cf1ee0
Tirumalesh Chalamarla <tchalamarla@caviumnetworks.com>
2017-03-01 19:00:24 -0800

    octeontx-rst: Add RST support
    Introducing RST driver needed for sso timeout.
    To enable driver set: CONFIG_OCTEONTX_RST=y
    
    Signed-off-by: Tirumalesh Chalamarla <tchalamarla@caviumnetworks.com>
    Signed-off-by: Santosh Shukla <santosh.shukla@caviumnetworks.com>

--- Kbuild
+++ Kbuild
@@ -1,4 +1,5 @@
-obj-m := fpapf.o fpavf.o
+obj-m := fpapf.o fpavf.o rst.o
 
 fpapf-y := fpapf_main.o
 fpavf-y := fpavf_main.o
+rst-y := rst_main.o
---
+++ rst.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 Cavium, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ */
+
+#ifndef RST_H
+#define RST_H
+
+#define PCI_DEVICE_ID_OCTEONTX_RST_PF	0xA00E
+
+#define PCI_RST_PF_CFG_BAR	0
+
+/* reg offset */
+#define RST_BOOT	0x1600
+
+#define PLL_REF_CLK	(50*1000*1000)
+
+struct rst_com_s {
+	u64 (*get_sclk_freq)(int id);
+	/* Will add rest function based on use-case */
+};
+extern struct rst_com_s rst_com;
+
+#endif
